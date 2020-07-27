214f4686af55ab223dfe926c60293ab9eba6fcad
mkdsrt10 <33832035+mkdsrt10@users.noreply.github.com>
2020-07-27 15:41:41 +0530

    Update the space error

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -5,7 +5,7 @@ const awsmobile = {
     "aws_project_region": "us-east-2",
     "aws_cognito_identity_pool_id": "us-east-2:23b48ebb-4a40-4740-9bc5-a7101a93efbf",
     "aws_cognito_region": "us-east-2",
-    "aws_user_pools_id": " us-east-2_zDc9DpPIy",
+    "aws_user_pools_id": "us-east-2_zDc9DpPIy",
     "aws_user_pools_web_client_id": "1i983q261ardqhlurfas375t6n",
     "oauth": {},
     "aws_user_files_s3_bucket": "helpdefeatcovid",
